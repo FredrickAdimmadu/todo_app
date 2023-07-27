34a4537e0a74eb8996b765149179040176adbade
FREDRICK ADIMMADU <64749432+FredrickAdimmadu@users.noreply.github.com>
2023-07-27 21:10:31 +0100

    todo app

---
+++ todo_app/lib/main.dart
@@ -0,0 +1,33 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:todo_app/auth/authscreen.dart';
+import 'package:todo_app/screens/home.dart';
+
+import 'firebase_options.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform); // Initialize Firebase
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: StreamBuilder<User?>(
+        stream: FirebaseAuth.instance.authStateChanges(),
+        builder: (context, usersnapshot) {
+          if (usersnapshot.hasData) {
+            return Home();
+          } else {
+            return AuthScreen();
+          }
+        },
+      ),
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(brightness: Brightness.dark, primaryColor: Colors.purple),
+    );
+  }
+}
